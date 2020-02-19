1db1d1a934ee1b486181feeb2eccd40ded7ba15a
Carsten Jacobsen <car.jacobsen@gmail.com>
2020-02-19 15:18:33 +0100

    style(index): refactor + prettier

---
+++ src/utils.js
@@ -0,0 +1,21 @@
+const ObjectId = require('mongoose').Types.ObjectId
+const { pathOr } = require('ramda')
+
+function ConvertToArray(val) {
+  return val instanceof Array ? val : [val]
+}
+
+function ExtractObjectId(val) {
+  return pathOr(val, ['_id'], val)
+}
+
+function CompareObjectId(a, b) {
+  a = ExtractObjectId(a)
+  b = ExtractObjectId(b)
+  return [a, b].every(v => v instanceof ObjectId) && a.equals(b)
+}
+
+module.exports = {
+  ConvertToArray,
+  CompareObjectId,
+}
